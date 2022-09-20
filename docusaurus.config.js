63ef5f61fe802ff51eefbb4d183df60d967bd0ea
Praveen Singh <praveenkumar_singh@intuit.com>
2022-09-20 15:10:29 +0530

    base url update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://ipraveen.github.io',
-  baseUrl: '/',
+  baseUrl: '/docusaurus-test-app/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
