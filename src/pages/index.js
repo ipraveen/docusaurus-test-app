629c4a789e32051bf75f699bd862e9f92d259aec
Praveen Singh <praveenkumar_singh@intuit.com>
2022-09-20 15:05:25 +0530

    cli publish

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Github Pages Try 1
           </Link>
         </div>
       </div>
